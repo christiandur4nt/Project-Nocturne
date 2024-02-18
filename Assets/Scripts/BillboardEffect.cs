685057defbe37650d2c315318b313a1b164c7c9a
christian.durant88@gmail.com <>
2024-02-18 18:59:20 -0500

    Add angle locking to billboard script.

--- Assets/Scripts/BillboardEffect.cs
+++ Assets/Scripts/BillboardEffect.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class BillboardEffect : MonoBehaviour
 {
+    [Header("Lock Rotation")]
+    [SerializeField] private bool lockX;
+    [SerializeField] private bool lockY;
+    [SerializeField] private bool lockZ;
+
+    private Vector3 originalRotation;
+
+    void Awake() {
+        originalRotation = transform.rotation.eulerAngles;
+    }
+
     void LateUpdate()
     {
         transform.LookAt(Camera.main.transform.position);
+
+        // For locking rotation
+        Vector3 rotation = transform.rotation.eulerAngles;
+        if (lockX) rotation.x = originalRotation.x;
+        if (lockY) rotation.y = originalRotation.y;
+        if (lockZ) rotation.z = originalRotation.z;
+        transform.rotation = Quaternion.Euler(rotation);
     }
 }
